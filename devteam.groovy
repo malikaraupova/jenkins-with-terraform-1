baedab8e02452eb899f24f3b29322950c50747a9
Emir Koroglu <69024539+emirsway@users.noreply.github.com>
2021-11-30 13:57:23 -0600

    groovy file added

---
+++ devteam.groovy
@@ -0,0 +1,39 @@
+
+
+node {
+    stage("Clone A Repository") {
+        timestamps {
+            checkout([$class: 'GitSCM', branches: [[name: '*/main']], extensions: [], userRemoteConfigs: [[url: 'https://github.com/emirsway/terraform-august-cloud.git']]])
+        }
+    }
+    stage("Initialize"){
+        timestamps {
+            sh 'terraform init'
+        }
+    }
+    stage("Validate"){
+        sh 'terraform validate'
+        }
+    }
+    stage("Plan"){
+        sh 'terraform plan'
+    }
+    stage("Confirmation"){
+        input 'Should I apply?'
+    }
+    stage("Apply"){
+        sh 'terraform apply -auto-approve'
+        echo 'running apply '
+    }
+    stage("Security Check"){
+        sh 'terraform refresh'
+    }
+    stage("Wait"){
+        sleep 5
+    }
+    stage("Email Notification to Team"){
+        mail bcc: 'EC2', body: 'EC2 is created in AWS', cc: 'EC2', from: '', replyTo: '', subject: 'EC2 Build', to: 'emirmails@gmail.com'
+    }
+    stage("Send message to a Contractor"){
+        mail bcc: '', body: '''Hi, VPC has been built Thanks''', cc: '', from: '', replyTo: '', subject: 'VPC being built', to: 'contractor@company.com'    }
+}
