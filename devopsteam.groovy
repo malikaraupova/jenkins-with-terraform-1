4709976b96aeeda717225f151ddb2805fb391ed6
malikaraupova <88691053+malikaraupova@users.noreply.github.com>
2021-12-17 15:04:36 -0600

    add

--- devopsteam.groovy
+++ devopsteam.groovy
@@ -16,7 +16,7 @@ node {
             sh 'terraform init'
         }
     }
-    stage("Confirmation"){
+     stage("Confirmation"){
         input 'Should I Destroy?'
     }
     stage("Wait"){
@@ -26,60 +26,60 @@ node {
         sh 'terraform destroy -auto-approve'
         echo 'running destroy'
     }
-    stage("Run Script"){
-        timestamps {
-        sh label: '', script: 
-		'''#!/bin/bash
-			if [ ! -d /tmp/foo ]; 
-			then
-				echo "Folder not found!"
-				echo "Creating a folder"
-				mkdir -p "/tmp/foo"
-			fi
-		'''
-        }
-    }
-    stage("Validate"){
-        sh 'terraform validate'
-    }
-    stage("Plan"){
-        sh 'terraform plan'
-    }
-    stage("Confirmation"){
-        input 'Should I apply?'
-    }
-    stage("Apply"){
-        sh 'terraform apply -auto-approve'
-        echo 'running apply '
-    }
-    stage("Security Check"){
-        sh label: '', script: 
-		'''#!/bin/bash
-			if [ ! -d /tmp/foo ]; 
-			then
-				echo "Folder not found!"
-				echo "Creating a folder"
-				mkdir -p "/tmp/foo"
-			fi
-		'''
-    }
-    stage("Wait"){
-        sleep 3
-    }
-    stage("Email Notification to Team"){
-        mail bcc: 'EC2-VPC Created', body: 'EC2-VPC Created is created in AWS', cc: 'EC2-VPC Created', from: 'emirmails@gmail.com', replyTo: 'emirmails@gmail.com', subject: 'EC2-VPC Created Build', to: 'emirmails@gmail.com'
-    }
-    stage("Send message to a Contractor"){
-        mail bcc: '', body: '''Hi, VPC has been built Thanks''', cc: '', from: '', replyTo: '', subject: 'VPC being built', to: 'contractor@company.com'
-    }
-    stage("Confirmation"){
-        input 'Should I Destroy?'
-    }
-    stage("Wait"){
-        sleep 3
-    }
-    stage("Destroy"){
-        sh 'terraform destroy -auto-approve'
-        echo 'running destroy'
-    }
-}
+//     stage("Confirmation"){
+//         input 'Should I Destroy?'
+//     }
+//     stage("Wait"){
+//         sleep 3
+//     }
+//     stage("Destroy"){
+//         sh 'terraform destroy -auto-approve'
+//         echo 'running destroy'
+//     }
+//     stage("Run Script"){
+//         timestamps {
+//         sh label: '', script: 
+// 		'''#!/bin/bash
+// 			if [ ! -d /tmp/foo ]; 
+// 			then
+// 				echo "Folder not found!"
+// 				echo "Creating a folder"
+// 				mkdir -p "/tmp/foo"
+// 			fi
+// 		'''
+//         }
+//     }
+//     stage("Validate"){
+//         sh 'terraform validate'
+//     }
+//     stage("Plan"){
+//         sh 'terraform plan'
+//     }
+//     stage("Confirmation"){
+//         input 'Should I apply?'
+//     }
+//     stage("Apply"){
+//         sh 'terraform apply -auto-approve'
+//         echo 'running apply '
+//     }
+//     stage("Security Check"){
+//         sh label: '', script: 
+// 		'''#!/bin/bash
+// 			if [ ! -d /tmp/foo ]; 
+// 			then
+// 				echo "Folder not found!"
+// 				echo "Creating a folder"
+// 				mkdir -p "/tmp/foo"
+// 			fi
+// 		'''
+//     }
+//     stage("Wait"){
+//         sleep 3
+//     }
+//     stage("Email Notification to Team"){
+//         mail bcc: 'EC2-VPC Created', body: 'EC2-VPC Created is created in AWS', cc: 'EC2-VPC Created', from: 'emirmails@gmail.com', replyTo: 'emirmails@gmail.com', subject: 'EC2-VPC Created Build', to: 'emirmails@gmail.com'
+//     }
+//     stage("Send message to a Contractor"){
+//         mail bcc: '', body: '''Hi, VPC has been built Thanks''', cc: '', from: '', replyTo: '', subject: 'VPC being built', to: 'contractor@company.com'
+//     }
+// }
